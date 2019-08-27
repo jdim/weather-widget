9c22743d5cbdbd6740bf7a0efc0531e6618fb22f
Dmitriy Khabarov <nixvod@gmail.com>
2019-08-28 00:22:21 +0300

    add link to github account

--- src/App.js
+++ src/App.js
@@ -11,16 +11,30 @@ function App() {
 
   return (
     <div className="App">
-      <h1 className="App-Header">Weather Widget</h1>
-      <Switch>
-        <Route
-          path={`/:selectedStatus(${statusesInPath})`}
-          component={WeatherWidget}
-        />
-        <Route
-          render={() => <Redirect to={`/${statuses.ALL.toLowerCase()}`} />}
-        ></Route>
-      </Switch>
+      <div className="App-Container">
+        <h1 className="App-Header">Weather Widget</h1>
+        <Switch>
+          <Route
+            path={`/:selectedStatus(${statusesInPath})`}
+            component={WeatherWidget}
+          />
+          <Route
+            render={() => <Redirect to={`/${statuses.ALL.toLowerCase()}`} />}
+          ></Route>
+        </Switch>
+        <p className="App-Github">
+          You can see code on{' '}
+          <a
+            className="App-GithubLink"
+            href="https://github.com/jdim/weather-widget"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            my GitHub{' '}
+            <img className="App-GithubImg" src="/GitHub-Mark-64px.png" alt="" />{' '}
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
